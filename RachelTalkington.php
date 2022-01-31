301935233ad1e75959b3d067aed3602907ce1b8b
rtalking10 <98490662+rtalking10@users.noreply.github.com>
2022-01-31 15:25:35 -0600

    Add files via upload

---
+++ RachelTalkington.php
@@ -0,0 +1,4 @@
+<?php
+nl2br(print("Howdy! My Name is Rachel Talkington. \nI am a Senior Technology Management Major with a Minor in Business. \nI am from Carrollton Texas."))
+echo "<img src='rachel.jpg' />";
+?>
